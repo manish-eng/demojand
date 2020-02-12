669c492f0d6a4f69733511127a6c495696b63173
manish.kumar <manish.kumar@digivalet.com>
2020-02-12 15:43:06 +0530

    added mail.py

---
+++ mail.py
@@ -0,0 +1,38 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+
+email_user = 'karansharmazz89@gmail.com'
+email_password = 'Roopktul'
+email_send = 'manish.kumar@digivalet.com,manishav962@gmail.com'
+
+subject = 'TEST'
+
+msg = MIMEMultipart()
+msg['From'] = email_user
+msg['To'] = email_send
+msg['Subject'] = subject
+
+body = 'Hi there, sending this email from Python!'
+msg.attach(MIMEText(body,'plain'))
+
+filename='/Users/manishkumar/fileScript.sh'
+attachment=open(filename,'rb')
+
+part = MIMEBase('application','octet-stream')
+part.set_payload((attachment).read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition',"attachment; filename= "+filename)
+
+msg.attach(part)
+text = msg.as_string()
+server = smtplib.SMTP('smtp.gmail.com',587)
+server.starttls()
+server.login(email_user,email_password)
+
+
+server.sendmail(email_user,email_send.split(','),text)
+server.quit()
+
